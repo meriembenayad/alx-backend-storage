16b036d702bf315c005df6e9fa33b1386a05e6e6
meriembenayad <meriem.benayad@gmail.com>
2024-03-20 23:30:47 +0000

    Task 0: We are all unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- 0. We are all unique!
+-- creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) Not NULL UNIQUE, name VARCHAR(255),
+);
