6273c4b9c5439cbf8e416f6d2b9142348b163f86
meriembenayad <meriem.benayad@gmail.com>
2024-03-21 02:04:56 +0000

    Task 8: Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,2 @@
 -- 8. Optimize simple search: Creates an index idx_name_first on the table names and the first letter of name.
-
-ALTER TABLE names ADD COLUMN first_letter CHAR(1);
-UPDATE names SET first_letter = (SUBSTRING(name, 1, 1));
-CREATE INDEX idx_name_first ON names(first_letter);
+CREATE INDEX idx_name_first ON names (name(1));
