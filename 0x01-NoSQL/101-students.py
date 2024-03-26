7f31808e2ade9bb4fca3483bcac8ee7dc5ac0b1d
meriembenayad <meriem.benayad@gmail.com>
2024-03-26 00:07:13 +0000

    Task 14: Top Students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 14. Top students """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    total_score = mongo_collection.aggregate([
+        {
+            '$students': {
+                'name': '$name',
+                'averageScore': {'$avg': '$topics.score'}
+            }
+        },
+        {
+            '$sort': {'averageScore': - 1}
+        }
+    ])
+    return total_score
